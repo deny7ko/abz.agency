bd8313b022a36d3e804b80b917d161287e71b1ee
Denys Medynskyi <denys.medynskyi@toptal.com>
2019-09-17 21:53:51 +0300

    Add public url

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,16 +3,16 @@ import React, { Component } from "react";
 class AboutMe extends Component {
   render() {
     return (
-      <div className='AboutMe d-flex justify-content-center '>
+      <div className="AboutMe d-flex justify-content-center ">
         <h1>Let’s get acquainted</h1>
-        <div className='mainContainer'>
-          <div className='d-flex flex-row bd-highlight justify-content-around m-5 p-3'>
+        <div className="mainContainer">
+          <div className="d-flex flex-row bd-highlight justify-content-around m-5 p-3">
             <img
-              src='/imgs/man-mobile.svg'
-              alt='logo'
-              className='p-2 bd-highlight'
+              src={`${process.env.PUBLIC_URL}/imgs/man-mobile.svg`}
+              alt="logo"
+              className="p-2 bd-highlight"
             />
-            <div className='mainTextAboutMe p-2 bd-highlight'>
+            <div className="mainTextAboutMe p-2 bd-highlight">
               <h4>I am cool frontend developer</h4>
               <h5>
                 When real users have a slow experience on mobile, they're much
@@ -27,7 +27,7 @@ class AboutMe extends Component {
                 initiatives to help improve user-experience on the web.
               </h5>
               <br />
-              <h4 className='signUpAboutMe'>Sign Up</h4>
+              <h4 className="signUpAboutMe">Sign Up</h4>
             </div>
           </div>
         </div>
