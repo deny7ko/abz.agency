a4dc0c7a75d63d694d7f48ab889701f28dfadd33
Denys Medynskyi <denys.medynskyi@toptal.com>
2019-09-17 21:46:35 +0300

    Updates

---
+++ precache-manifest.bf0b64cbac162d1e4c4ec066d96a4c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b592ee871e2a08bbf3977bf306183c3",
+    "url": "/abz.agency/index.html"
+  },
+  {
+    "revision": "4084b7159aa75254ad4e",
+    "url": "/abz.agency/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "034cf38e8b91c84f5d63",
+    "url": "/abz.agency/static/css/main.e9b792d6.chunk.css"
+  },
+  {
+    "revision": "4084b7159aa75254ad4e",
+    "url": "/abz.agency/static/js/2.e86b3e93.chunk.js"
+  },
+  {
+    "revision": "034cf38e8b91c84f5d63",
+    "url": "/abz.agency/static/js/main.8c21bf20.chunk.js"
+  },
+  {
+    "revision": "cff38c7267e1f9f72bf6",
+    "url": "/abz.agency/static/js/runtime~main.ac32843d.js"
+  }
+]);
