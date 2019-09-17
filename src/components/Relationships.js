33afaf1e9d5d792cecbb87058f39950c6d9ff694
Denys Medynskyi <denys.medynskyi@toptal.com>
2019-09-17 22:04:58 +0300

    Add HTML svg to the src folder

--- src/components/Relationships.js
+++ src/components/Relationships.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
+import HTMLIcon from "../imgs/html.svg"
 
 class Relationships extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className='titleSkills d-flex justify-content-center text-center'>
+        <div className="titleSkills d-flex justify-content-center text-center">
           <h1>
             About my relationships with
             <br /> web-development
           </h1>
         </div>
-        <div className='titles d-grid mt-5'>
-          <div className='row mr-auto ml-auto'>
-            <div className='col-sm'>
-              <img src='imgs/html.svg' logo='html' />
+        <div className="titles d-grid mt-5">
+          <div className="row mr-auto ml-auto">
+            <div className="col-sm">
+              <img src={HTMLIcon} logo="html" />
               <h4>I’m in love with HTML</h4>
               <h5>
                 Hypertext Markup Language(HTML) is the standard markup language
                 for creating web pages and web applications.
               </h5>
             </div>
-            <div className='col-sm'>
-              <img src='imgs/css.svg' logo='css' />
+            <div className="col-sm">
+              <img src="imgs/css.svg" logo="css" />
               <h4>CSS is my best friend</h4>
               <h5>
                 Cascading Style Sheets(CSS) is a style sheet language used for
@@ -29,8 +30,8 @@ class Relationships extends Component {
                 language like HTML
               </h5>
             </div>
-            <div className='col-sm'>
-              <img src='imgs/javascript.svg' logo='javascript' />
+            <div className="col-sm">
+              <img src="imgs/javascript.svg" logo="javascript" />
               <h4>JavScript is my passion</h4>
               <h5>
                 JavaScript is a high-level, interpreted programming language. It
